372ec61b18dd35053c1f484b64ec978edb72b4f2
I A <ilie.adrian1986@gmail.com>
2023-09-05 16:20:09 +0300

    basic calculation functions and operate function added

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello world")
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(operator, a, b){
+    a = parseFloat(a);
+    b = parseFloat(b);
+    if (operator === "/") {
+        divide(a, b);
+    } else if (operator === "*") {
+        multiply(a, b);
+    } else if (operator === "-") {
+        subtract(a, b);
+    } else {
+        add(a, b);
+    }
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function add(a, b) {
+    return a + b;
+}
