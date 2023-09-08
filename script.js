5776ee6c903b3a47910fe0fc1c81208d0ff6e033
I A <ilie.adrian1986@gmail.com>
2023-09-08 17:07:06 +0300

    Javascript for number and operator inputs, numbers are formed in to an array until an operator is pressed, basic visualisation added

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-let firstNumber;
-let secondNumber;
+let firstNumber = [];
+let secondNumber = [];
 let operator;
+let clickedNumber;
+let clickedOperator;
+let firstToDisplay;
+let secondToDisplay;
+let storingInFirstNumber = true;
+let calculationValue = document.getElementById("calculationValue");
+let currentValue = document.getElementById("currentValue");
+const numbers = document.querySelectorAll("[data-number]");
+const operators = document.querySelectorAll("[data-operator]");
+
+numbers.forEach((element) => {
+    element.addEventListener("click", getNumber);
+});
+
+function getNumber() {
+    clickedNumber = event.target.textContent;
+    computeNumbers()
+}
+
+operators.forEach((element) => {
+    element.addEventListener("click", getOperator);
+});
+
+function getOperator(){
+    operator = event.target.textContent;
+    if (operator) {
+        storingInFirstNumber = false;
+    }
+}
+
+function computeNumbers() {
+    if (storingInFirstNumber) {
+        firstNumber.push(clickedNumber);
+        updateDisplay(firstToDisplay = firstNumber.join("").toString());
+    } else {
+        secondNumber.push(clickedNumber);
+        updateDisplay(secondToDisplay = secondNumber.join("").toString());
+    }
+
+    console.log(firstNumber, firstToDisplay)
+}
 
 function operate(operator, a, b){
     a = parseFloat(a);
@@ -31,3 +72,10 @@ function subtract(a, b) {
 function add(a, b) {
     return a + b;
 }
+
+function updateDisplay(){
+    currentValue.innerHTML = `${firstToDisplay || 0} ${operator || ''}  ${secondToDisplay || ''}`;
+    console.log(firstNumber, firstToDisplay)
+}
+
+updateDisplay();
