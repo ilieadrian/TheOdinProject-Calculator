35dfaef374bfa46f8845d0943567acbb112838d9
I A <ilie.adrian1986@gmail.com>
2023-09-19 19:20:21 +0300

    work at warious functions

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ let firstToDisplay;
 let secondToDisplay;
 let storingInFirstNumber = true;
 let result;
-let randomCheck = false;
+let checkForSecondOperator = false;
 let operatorCount = 0;
 let calculationValue = document.getElementById("calculationValue");
 let currentValue = document.getElementById("currentValue");
 const numbers = document.querySelectorAll("[data-number]");
 const operators = document.querySelectorAll("[data-operator]");
+let clearBtn = document.querySelector(".clear");
 
 numbers.forEach((element) => {
     element.addEventListener("click", getNumber);
@@ -36,7 +37,7 @@ function getOperator(){
 
         if(secondNumber.length !== 0 && operatorCount > 1) {
             operate(operator, firstToDisplay, secondToDisplay);
-            randomCheck = true;
+            checkForSecondOperator = true;
             updateDisplay();
         }
     }
@@ -89,10 +90,22 @@ function add(a, b) {
 function updateDisplay(){
     currentValue.innerHTML = `${firstToDisplay || 0} ${operator || ''} ${secondToDisplay || ''}`;
 
-    if (randomCheck){
+    if (checkForSecondOperator){
         currentValue.innerHTML = `${result}`;
     }
     
 }
 
+function clear() {
+    firstNumber.splice(0,firstNumber.length);
+    secondNumber.splice(0,secondNumber.length);
+    storingInFirstNumber = true;
+    result = 0;
+    checkForSecondOperator = false;
+    operatorCount = 0;
+    operator = null;
+    currentValue.innerHTML = `0`;
+}
+
 updateDisplay();
+clearBtn.addEventListener("click", clear);
